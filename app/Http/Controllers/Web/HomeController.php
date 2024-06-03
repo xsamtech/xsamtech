509bf63786e3bead9f604fed128c91bb4c03cf27
xanderssamoth <124665689+xsamtech@users.noreply.github.com>
2024-06-03 12:46:41 +0100

    n

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -2,10 +2,49 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Http\Controllers\ApiClientManager;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * @author Xanders
+ * @see https://team.xsamtech.com/xanderssamoth
+ */
 class HomeController extends Controller
 {
-    //
+    public static $api_client_manager;
+
+    public function __construct()
+    {
+        $this::$api_client_manager = new ApiClientManager();
+    }
+
+    // ==================================== HTTP GET METHODS ====================================
+    /**
+     * GET: Change language
+     *
+     * @param  $locale
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function changeLanguage($locale)
+    {
+        app()->setLocale($locale);
+        session()->put('locale', $locale);
+
+        return redirect()->back();
+    }
+
+    /**
+     * GET: Welcome/Home page
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        return view('welcome');
+    }
+
+    // ==================================== HTTP POST METHODS ====================================
+
 }
